e52fc2cd5dbfd66adbf2488509606a7ac7e64bf8
Amit Kumar <amitgarg.cse@gmail.com>
2020-09-10 14:37:43 +0530

    promise error route

---
+++ promises/promise-error.ts
@@ -0,0 +1,9 @@
+const nodeFetch = require('node-fetch');
+
+nodeFetch('https://no-such-server.blabla') // rejects
+    .then((response: any) => response.json())
+    .catch((err: any) =>
+    {
+        console.log('failed to fetch data from this url');
+        console.error(err)
+    });
